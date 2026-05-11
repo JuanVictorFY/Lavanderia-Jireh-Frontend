e11b801e7dafdfbe801b367f733c6f83c73deaa3
JuanVictorFY <figyuptonj99@gmail.com>
2026-05-11 10:00:00 -0500

    chore(hooks): add barrel export index

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+export { useConfirm }      from "./useConfirm";
+export { useDebounce }     from "./useDebounce";
+export { useLocalStorage } from "./useLocalStorage";
+export { usePagination }   from "./usePagination";
+export { useSort }         from "./useSort";
+export { useTitle }        from "./useTitle";
